fa820ca981112c3338de42a18acd79abfd94e4c0
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2022-09-01 02:10:34 +0200

    sorting results

--- WordCount/ClientApp/src/app/results-view/results-view.component.ts
+++ WordCount/ClientApp/src/app/results-view/results-view.component.ts
@@ -7,14 +7,27 @@ import { StateHelperService } from 'src/core/state-helper.service';
   styleUrls: ['./results-view.component.scss']
 })
 export class ResultsViewComponent implements OnInit {
-  WordCountData?: object;
+  WordCountData?: {
+    x: string;
+    y: any;
+}[];
 
   constructor(private stateHelperService: StateHelperService) { }
 
   ngOnInit(): void {
-    this.stateHelperService.WordCount$.subscribe(wordcountData=> {
-      console.log(wordcountData); 
-      this.WordCountData = wordcountData;
+    this.stateHelperService.WordCount$.subscribe(wordData=> {
+      const justForReq = []
+      for (const word in wordData) {
+        if (Object.prototype.hasOwnProperty.call(wordData, word)) {
+          const count = (wordData as any)[word];
+          justForReq.push({ x: word, y: count });
+          justForReq.sort((b, a)=>  {
+            return parseFloat(a.y) - parseFloat(b.y);
+          });
+          
+        }
+      }
+      this.WordCountData = justForReq
     })
   }
   keys(value: object) {
